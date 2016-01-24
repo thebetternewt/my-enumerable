d0e27dab679581145621217fe40bf7f8db11fec5
Chris Eady <chris.eady1@gmail.com>
2016-01-24 15:29:06 -0600

    added my_select method

--- my-enumerable.rb
+++ my-enumerable.rb
@@ -1,19 +1,33 @@
 module Enumerable
   def my_each
-    i = 0
-    while i < self.size
-      yield(self[i])
-      i += 1
+    if block_given?
+      i = 0
+      while i < self.size
+        yield(self[i])
+        i += 1
+      end
     end
     self
   end
 
   def my_each_with_index
-    i = 0
-    while i < self.size
-      yield(self[i], i)
-      i += 1
+    if block_given?
+      i = 0
+      while i < self.size
+        yield(self[i], i)
+        i += 1
+      end
     end
     self
+  end
+
+  def my_select
+    arr = []
+    if block_given?
+      self.my_each { |item| yield(item) ? arr << item : nil}
+      arr
+    else
+      self
+    end
   end
 end
