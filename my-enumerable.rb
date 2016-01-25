9611d07f628bcf164cbbb2779aa6b129b69c7fd2
Chris Eady <chris.eady1@gmail.com>
2016-01-25 11:41:14 -0600

    added my_all? and my_none? methods

--- my-enumerable.rb
+++ my-enumerable.rb
@@ -30,4 +30,32 @@ module Enumerable
       self
     end
   end
+
+  def my_all?
+    if block_given?
+      self.my_each { |item| yield(item) ? nil : (return false) }
+      true
+    else
+      true
+    end
+  end
+
+  def my_any?
+    if block_given?
+      self.my_each { |item| yield(item) ? (return true) : nil }
+      false
+    else
+      true
+    end
+  end
+
+  def my_none?
+    if block_given?
+      self.my_each { |item| yield(item) ? (return false) : nil }
+      true
+    else
+      false
+    end
+  end
+
 end
